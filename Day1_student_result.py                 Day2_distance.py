fb27447edda9205e5d4312df948e7297d225f008
Nakshathra-17S <cgrshiva@gmail.com>
2026-02-21 13:20:26 +0530

    Implement distance calculation between two numbers
    Added a script to calculate the distance between two numbers using the absolute value function. 
    #a new beginning

---
+++ Day1_student_result.py                 Day2_distance.py
@@ -0,0 +1,18 @@
+#printing student result using formatted string
+
+name =" nakshathra "
+marks= 92
+print(f" {nakshathra} scored {92} marks in mathematics")
+
+
+
+
+#using absolute value function calculating diatnce betwen two numbers
+a=12
+b=34
+difference=abs(a-b)
+print(difference)
+
+
+#output-------- nakshathra scored 92 marks in mathematics
+#      --------  the difference between two numvers is 22
